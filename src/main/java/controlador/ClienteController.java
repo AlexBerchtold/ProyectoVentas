b07776ecaabf797dae097bac67e244dee218db3f
alex bryan berchtol pereira <alexbryanberchtol@gmail.com>
2026-06-09 20:58:28 -0300

    Agregado metodo de estado inicial y guardar en cliente controller

--- src/main/java/controlador/ClienteController.java
+++ src/main/java/controlador/ClienteController.java
@@ -1,43 +1,110 @@
 package controlador;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.List;
 
+import dao.ClienteDAO;
 import interfaces.InterfaceABM;
+import modelo.ClienteModelo;
 import util.FechaUtil;
 import vista.ClienteVista;
 
 public class ClienteController implements InterfaceABM {
-	
+
 	private ClienteVista vista;
+	private ClienteModelo cliente;
+	private ClienteDAO dao;
+	private List<ClienteModelo> clientes;
 
 	public ClienteController(ClienteVista clienteVista) {
 		super();
 		this.vista = clienteVista;
 		this.vista.setInterfaceABM(this);
-		
-		vista.getTfFecha().setText(FechaUtil.fechaAString(LocalDate.now()));
-		
-		vista.getTfHora().setText(FechaUtil.horaAString(LocalTime.now()));
-		
+		dao = new ClienteDAO();
+		estadoInicial();
+	}
+
+	private void estadoInicial() {
+		// desactiva los botones
+		this.vista.getBtnNuevo().setEnabled(true);
+		this.vista.getBtnEditar().setEnabled(false);
+		this.vista.getBtnEliminar().setEnabled(false);
+		this.vista.getBtnCancelar().setEnabled(true);
+		this.vista.getBtnGuardar().setEnabled(false);
+
+		// desactiva los textfield
+		this.vista.getTfFechaRegistro().setEnabled(false);
+		this.vista.getTfNombre().setEnabled(false);
+		this.vista.getTfApellido().setEnabled(false);
+		this.vista.getTfDocumento().setEnabled(false);
+		this.vista.getTfFechaNacimiento().setEnabled(false);
+		this.vista.getTfCorreo().setEnabled(false);
+		this.vista.getTfDireccion().setEnabled(false);
+		this.vista.getTfTelefono().setEnabled(false);
+
+		// Limpiar los campos
+		this.vista.getTfFechaRegistro().setValue(null);
+		this.vista.getTfFechaNacimiento().setValue(null);
+		this.vista.getTfNombre().setText("");
+		this.vista.getTfApellido().setText("");
+		this.vista.getTfDocumento().setText("");
+		this.vista.getTfCorreo().setText("");
+		this.vista.getTfDireccion().setText("");
+		this.vista.getTfTelefono().setText("");
+		cliente = null;
+
 	}
 
 	@Override
 	public void nuevo() {
-		System.out.println(vista.getTfFecha().getText());
+		// desactiva los botones
+		this.vista.getBtnNuevo().setEnabled(false);
+		this.vista.getBtnEditar().setEnabled(false);
+		this.vista.getBtnEliminar().setEnabled(false);
+		this.vista.getBtnCancelar().setEnabled(true);
+		this.vista.getBtnGuardar().setEnabled(true);
+
+		// desactiva los textfield
+		this.vista.getTfFechaRegistro().setEnabled(false);
+		this.vista.getTfNombre().setEnabled(true);
+		this.vista.getTfApellido().setEnabled(true);
+		this.vista.getTfDocumento().setEnabled(true);
+		this.vista.getTfFechaNacimiento().setEnabled(true);
+		this.vista.getTfCorreo().setEnabled(true);
+		this.vista.getTfDireccion().setEnabled(true);
+		this.vista.getTfTelefono().setEnabled(true);
 		
+		//Carga el campo fecha y crea el cliente
+		cliente = new ClienteModelo();
+		this.vista.getTfFechaRegistro().setText(FechaUtil.fechaAString(LocalDate.now()));
+
 	}
 
 	@Override
 	public void editar() {
 		System.out.println("editar");
-		
+
 	}
 
 	@Override
 	public void guardar() {
-		System.out.println("Guardar");
 		
+		cliente.setFechaRegistro(FechaUtil.stringAFecha(this.vista.getTfFechaRegistro().getText()));
+		cliente.setFechaNacimiento(FechaUtil.stringAFecha(this.vista.getTfFechaNacimiento().getText()));
+		cliente.setNombre(this.vista.getTfNombre().getText());
+		cliente.setApellido(this.vista.getTfApellido().getText());
+		cliente.setDocumento(this.vista.getTfDocumento().getText());
+		cliente.setTelefono(this.vista.getTfTelefono().getText());
+		cliente.setCorreo(this.vista.getTfCorreo().getText());
+		cliente.setDireccion(this.vista.getTfDireccion().getText());
+		
+		try {
+			dao.guardar(cliente);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
 	}
 
 	@Override
@@ -47,7 +114,8 @@ public class ClienteController implements InterfaceABM {
 
 	@Override
 	public void cancelar() {
-		System.out.println("cancelar");
+		if(cliente == null) this.vista.dispose();
+		else estadoInicial();
 	}
 
 	@Override
--- src/main/java/vista/ClienteVista.java
+++ src/main/java/vista/ClienteVista.java
@@ -9,12 +9,20 @@ import javax.swing.JLabel;
 import componentes.JDialogGenerico;
 import controlador.ClienteController;
 import util.FechaUtil;
+import componentes.JLabelGenerico;
+import componentes.JtextFieldGenerico;
 
 public class ClienteVista extends JDialogGenerico {
 
 	private static final long serialVersionUID = 1L;
-	private JFormattedTextField tfFecha;
-	private JFormattedTextField tfHora;
+	private JFormattedTextField tfFechaRegistro;
+	private JtextFieldGenerico tfNombre;
+	private JtextFieldGenerico tfApellido;
+	private JtextFieldGenerico tfDocumento;
+	private JFormattedTextField tfFechaNacimiento;
+	private JtextFieldGenerico tfTelefono;
+	private JtextFieldGenerico tfCorreo;
+	private JtextFieldGenerico tfDireccion;
 
 	/**
 	 * Launch the application.
@@ -43,36 +51,116 @@ public class ClienteVista extends JDialogGenerico {
 	 */
 	public ClienteVista() {
 		
-		JLabel lblNewLabel = new JLabel("Fecha:");
-		lblNewLabel.setBounds(20, 23, 87, 18);
-		getPanelFormulario().add(lblNewLabel);
+		JLabelGenerico lblgnrcNombre = new JLabelGenerico((String) null);
+		lblgnrcNombre.setText("Nombre:");
+		lblgnrcNombre.setBounds(10, 74, 142, 25);
+		getPanelFormulario().add(lblgnrcNombre);
 		
-		tfFecha = new JFormattedTextField(FechaUtil.getFormatoFecha());
-		tfFecha.setBounds(128, 23, 87, 29);
-		getPanelFormulario().add(tfFecha);
-		tfFecha.setColumns(10);
+		JLabelGenerico lblgnrcApellido = new JLabelGenerico((String) null);
+		lblgnrcApellido.setText("Apellido:");
+		lblgnrcApellido.setBounds(10, 124, 142, 25);
+		getPanelFormulario().add(lblgnrcApellido);
 		
-		tfHora = new JFormattedTextField(FechaUtil.getFormatoHora());
-		tfHora.setColumns(10);
-		tfHora.setBounds(270, 23, 87, 29);
-		getPanelFormulario().add(tfHora);
+		JLabelGenerico lblgnrcDocumento = new JLabelGenerico((String) null);
+		lblgnrcDocumento.setText("Documento:");
+		lblgnrcDocumento.setBounds(10, 178, 142, 25);
+		getPanelFormulario().add(lblgnrcDocumento);
+		
+		JLabelGenerico lblgnrcTelefono = new JLabelGenerico((String) null);
+		lblgnrcTelefono.setText("Telefono:");
+		lblgnrcTelefono.setBounds(10, 285, 142, 25);
+		getPanelFormulario().add(lblgnrcTelefono);
+		
+		JLabelGenerico lblgnrcCorreo = new JLabelGenerico((String) null);
+		lblgnrcCorreo.setText("Correo:");
+		lblgnrcCorreo.setBounds(10, 340, 142, 25);
+		getPanelFormulario().add(lblgnrcCorreo);
+		
+		JLabelGenerico lblgnrcDireccin = new JLabelGenerico((String) null);
+		lblgnrcDireccin.setText("Dirección:");
+		lblgnrcDireccin.setBounds(10, 391, 142, 25);
+		getPanelFormulario().add(lblgnrcDireccin);
+		
+		JLabelGenerico lblgnrcFechaNacimiento = new JLabelGenerico((String) null);
+		lblgnrcFechaNacimiento.setText("Fecha Nacimiento:");
+		lblgnrcFechaNacimiento.setBounds(10, 227, 142, 25);
+		getPanelFormulario().add(lblgnrcFechaNacimiento);
+		
+		JLabelGenerico lblgnrcFechaRegistro = new JLabelGenerico((String) null);
+		lblgnrcFechaRegistro.setText("Fecha Registro:");
+		lblgnrcFechaRegistro.setBounds(10, 28, 142, 25);
+		getPanelFormulario().add(lblgnrcFechaRegistro);
+		
+		tfFechaRegistro = new JFormattedTextField(FechaUtil.getFormatoFecha());
+		tfFechaRegistro.setBounds(162, 28, 100, 25);
+		getPanelFormulario().add(tfFechaRegistro);
+		
+		tfNombre = new JtextFieldGenerico();
+		tfNombre.setBounds(162, 74, 343, 25);
+		getPanelFormulario().add(tfNombre);
+		
+		tfApellido = new JtextFieldGenerico();
+		tfApellido.setBounds(162, 124, 343, 25);
+		getPanelFormulario().add(tfApellido);
+		
+		tfDocumento = new JtextFieldGenerico();
+		tfDocumento.setBounds(162, 178, 172, 25);
+		getPanelFormulario().add(tfDocumento);
+		
+		tfFechaNacimiento = new JFormattedTextField(FechaUtil.getFormatoFecha());
+		tfFechaNacimiento.setBounds(162, 227, 100, 25);
+		getPanelFormulario().add(tfFechaNacimiento);
+		
+		tfTelefono = new JtextFieldGenerico();
+		tfTelefono.setBounds(162, 285, 172, 25);
+		getPanelFormulario().add(tfTelefono);
+		
+		tfCorreo = new JtextFieldGenerico();
+		tfCorreo.setBounds(162, 340, 343, 25);
+		getPanelFormulario().add(tfCorreo);
+		
+		tfDireccion = new JtextFieldGenerico();
+		tfDireccion.setBounds(162, 391, 343, 25);
+		getPanelFormulario().add(tfDireccion);
 		
 
 	}
 
-	public JFormattedTextField getTfFecha() {
-		return tfFecha;
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 
-	public void setTfFecha(JFormattedTextField tfFecha) {
-		this.tfFecha = tfFecha;
+	public JFormattedTextField getTfFechaRegistro() {
+		return tfFechaRegistro;
 	}
 
-	public JFormattedTextField getTfHora() {
-		return tfHora;
+	public JtextFieldGenerico getTfNombre() {
+		return tfNombre;
 	}
 
-	public void setTfHora(JFormattedTextField tfHora) {
-		this.tfHora = tfHora;
+	public JtextFieldGenerico getTfApellido() {
+		return tfApellido;
 	}
+
+	public JtextFieldGenerico getTfDocumento() {
+		return tfDocumento;
+	}
+
+	public JFormattedTextField getTfFechaNacimiento() {
+		return tfFechaNacimiento;
+	}
+
+	public JtextFieldGenerico getTfTelefono() {
+		return tfTelefono;
+	}
+
+	public JtextFieldGenerico getTfCorreo() {
+		return tfCorreo;
+	}
+
+	public JtextFieldGenerico getTfDireccion() {
+		return tfDireccion;
+	}
+	
+	
 }
